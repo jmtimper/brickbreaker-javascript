e27596197a0520ac3cf175f32ecfd5cce2c818f4
jmtimper <jmtimper@ncsu.edu>
2018-01-15 02:03:29 -0500

    #2 Creates canvas when button is pressed

--- BrickBreaker/script/game.js
+++ BrickBreaker/script/game.js
@@ -1,9 +1,30 @@
-//request animation frame that runs the game at 60fps
 var animate = window.requestAnimationFrame ||
     window.webkitRequestAnimationFrame ||
     window.mozRequestAnimationFrame ||
     function(callback) { window.setTimeout(callback, 1000/60) };
 
-function startGame(){
+var canvas = document.createElement('canvas');
+var width = 400;
+var height = 600;
+canvas.width = width;
+canvas.height = height;
+var ctx = canvas.getContext('2d');
 
-}
+function startGame() {
+    document.body.appendChild(canvas);
+    animate(step);
+};
+
+var step = function() {
+    update();
+    render();
+    animate(step);
+};
+
+var update = function() {
+};
+
+var render = function() {
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(0, 0, width, height);
+};
