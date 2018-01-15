fc96bf27cf5e0524940319aeda2d527d858ae5c9
jmtimper <jmtimper@ncsu.edu>
2018-01-14 23:24:26 -0500

    Initial Commit with css play button and base script/html

---
+++ BrickBreaker/script/game.js
@@ -0,0 +1,9 @@
+//request animation frame that runs the game at 60fps
+var animate = window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    function(callback) { window.setTimeout(callback, 1000/60) };
+
+function startGame(){
+
+}
